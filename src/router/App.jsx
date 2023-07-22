228b9c517141c4fb6a5ff0153dba45e74c49566e
Cristian Espejel <tapiaxto@gmail.com>
2023-07-21 18:15:52 -0600

    ajustes de proyecto

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,14 +1,19 @@
-import {} from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
+import Header from '../components/Header/Header'
+import Home from '../pages/Home'
+
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route  path='/' element={<Home/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
